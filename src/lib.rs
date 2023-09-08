3d9c5c2b763fd7429105dc926a813b27b524220a
Huy Nguyen Quang <39359294+quanghuynguyen1902@users.noreply.github.com>
2023-09-08 21:58:05 +0700

    test

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+/// This is a simple public function that adds two numbers.
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!(my_public_crate::add(2, 3), 5);
+/// ```
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
